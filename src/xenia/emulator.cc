3f64059017e87c9213ba74d5ff9e3288dd62c391
Codie Newark <a13x231mcsl@gmail.com>
2024-09-16 17:00:22 +0900

    missed.

--- src/xenia/emulator.cc
+++ src/xenia/emulator.cc
@@ -1195,7 +1195,7 @@ X_STATUS Emulator::CompleteLaunch(const std::filesystem::path& path,
   // to make sure any HostPathDevices are ready beforehand. (see comment above
   // cache:\ device registration for more info about why)
   auto null_paths = {std::string("\\Partition0"), std::string("\\Cache0"),
-                     std::string("\\Cache1"), std::string("\\MU")};
+                     std::string("\\Cache1"), std::string("\\Mu0")};
   auto null_device =
       std::make_unique<vfs::NullDevice>("\\Device\\Harddisk0", null_paths);
   if (null_device->Initialize()) {
